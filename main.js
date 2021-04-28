6446935cf5fd1e9c82630360c547eca852547af2
Viral <viralrbhadeshiya@gmail.com>
2021-04-28 11:15:08 +0530

    [Feature-Branch]: Logic for number to string without for basics

--- main.js
+++ main.js
@@ -1,2 +1,37 @@
+// const for identify no string
+const numberMap = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine'
+}
+
+// std input
 const args = process.argv.slice(2)
-console.log(args)
+const result = []
+args.forEach(no => {
+    if(no){
+        let string = null;
+        if(no > 9){
+            while (no > 9){
+                singleDigit = (no % 10)
+                if(singleDigit === 0){
+                    no = no/10
+                } else {
+                    no = no - singleDigit
+                    string = (numberMap[singleDigit]) + string
+                }
+            }
+        }
+        string = numberMap[no] + string
+        newString = string.replace('null', '')
+        result.push(newString)
+    }
+});
+console.log(result)
