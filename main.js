69efe439f5634702c6c2694e587dead394687dfd
Viral <viralrbhadeshiya@gmail.com>
2021-04-28 11:31:46 +0530

    [Feature_Branch]: Final script with all tests

--- main.js
+++ main.js
@@ -17,21 +17,26 @@ const args = process.argv.slice(2)
 const result = []
 args.forEach(no => {
     if(no){
-        let string = null;
-        if(no > 9){
-            while (no > 9){
-                singleDigit = (no % 10)
-                if(singleDigit === 0){
-                    no = no/10
-                } else {
-                    no = no - singleDigit
-                    string = (numberMap[singleDigit]) + string
-                }
-            }
-        }
-        string = numberMap[no] + string
-        newString = string.replace('null', '')
-        result.push(newString)
+        let intArray = String(no).split('').map((num) => {
+            return numberMap[Number(num)];
+        });
+        intString = intArray.toString().replace(/,/g, '');
+        result.push(intString);
+        // let string = null;
+        // if(no > 9){
+        //     while (no > 9){
+        //         singleDigit = (no % 10)
+        //         if(singleDigit === 0){
+        //             no = no/10
+        //         } else {
+        //             no = no - singleDigit
+        //             string = (numberMap[singleDigit]) + string
+        //         }
+        //     }
+        // }
+        // string = numberMap[no] + string
+        // newString = string.replace('null', '')
+        // result.push(newString)
     }
 });
 console.log(result)
